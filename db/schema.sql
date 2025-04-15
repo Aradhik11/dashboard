cf370e96dc81a3fa32a96170c748a266ee6adaf1
Dhikrullah Abdur-Rahmon <aradhik11@gmail.com>
2025-04-15 21:20:48 +0100

    bug fix

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS accounts (
   email VARCHAR(255) NOT NULL UNIQUE,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
+  phone VARCHAR(20),
+  address TEXT,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 );
